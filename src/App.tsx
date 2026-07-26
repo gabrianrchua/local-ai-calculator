f058ca8a21e3db238c32b08ef39db61e3d270096
Gabrian Chua <gabrianrchua@gmail.com>
2026-07-25 22:33:45 -0400

    feat: make dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import './App.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import HomePage from './pages/Home';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 export const App = () => {
-  return <div>hello world</div>;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <HomePage />
+    </ThemeProvider>
+  );
 };
 
 export default App;
